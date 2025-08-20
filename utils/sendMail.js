dece63027f881242aded93bec7dbe45cb290ce57
Abtikdigital <rbhensla1994@gmail.com>
2025-08-20 11:00:00 +0530

    updated backedn

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,26 +1,31 @@
-const nodemailer = require("nodemailer")
-
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    auth: {
-        user: process?.env?.firmMail,
-        pass: process?.env?.firmPass
-    },
-    service: "gmail"
-})
+  host: process?.env?.SMTP_HOST_NAME,
+  port: process?.env?.SMTP_PORT,
+  secure: process?.env?.SMTP_SECURE,
+  auth: {
+    user: process?.env?.firmMail,
+    pass: process?.env?.firmPass,
+  },
+});
 
-
-
-const sendMail = async (from, to, subject, template,attachments) => {
-    try {
-        let isSended =await transporter.sendMail({ to, from, subject, html: template,attachments})
-        if (isSended) {
-            console.log("Mail Sended Successfully")
-        } else {
-            console.log("Error while sending mail")
-        }
-    } catch (error) {
-console.log(error)
+const sendMail = async (from, to, subject, template, attachments) => {
+  try {
+    let isSended = await transporter.sendMail({
+      to,
+      from,
+      subject,
+      html: template,
+      attachments,
+    });
+    if (isSended) {
+      console.log("Mail Sended Successfully");
+    } else {
+      console.log("Error while sending mail");
     }
-}
-module.exports = { sendMail }
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = { sendMail };
